e240873eabe3016dca9c74ba2d66f2e6237ad622
sigve.indregard <sigve.indregard@d6cfbd13-a94d-0410-ab2d-11010200fafc>
2008-05-20 13:23:21 +0000

    Importing exisiting code.

---
+++ app/controllers/calendar_controller.rb
@@ -0,0 +1,26 @@
+class CalendarController < ApplicationController
+  caches_page :matches
+
+  def matches
+    @fixtures = Fixtures.new
+    rows = get_table_rows(params[:id])
+    @fixtures.from_table_rows(rows)
+
+    respond_to do |format|
+      format.html
+      format.ics {render :text => @fixtures.to_ical}
+    end
+  end
+
+
+  private
+  def get_table_rows(team_id)
+    require 'hpricot'
+    require 'open-uri'
+
+    # TODO: Check sanity of team_id
+    doc = Hpricot(open("http://idrett.speaker.no/07/organisation.aspx?WCI=wiTeamResults&WCU=#{team_id}&HideKO=y"))
+
+    doc/"table.tblFixtures"/"tr.even"
+  end
+end
