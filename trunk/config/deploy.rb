9618f5eb0aff5e11456c7fcf4193b038ec292bc7
sigve.indregard <sigve.indregard@d6cfbd13-a94d-0410-ab2d-11010200fafc>
2009-01-13 14:44:14 +0000

    Update application to rails 2.2 and change cap file to do deployment to new server.

--- trunk/config/deploy.rb
+++ trunk/config/deploy.rb
@@ -1,18 +1,19 @@
 set :application, "speakercal"
+set :deploy_to, "/home/sigvei/public_html/railsapps/#{application}"
 set :repository,  "http://speakercal.googlecode.com/svn/trunk"
+
+set :use_sudo, true
+
 set :user, 'sigvei'
-set :use_sudo, false
-set :runner, :user
+set :domain, "speakercal.trylle.no"
+server domain, :app, :web
+role :db, domain, :primary => true
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-set :deploy_to, "/users/home/sigvei/railsapps/#{application}"
+namespace :passenger do
+  desc "Restart application"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
-
-role :app, "sigvei@trylle.no"
-role :web, "sigvei@trylle.no"
-role :db,  "sigvei@trylle.no", :primary => true
+after :deploy, "passenger:restart"
